39883fff4fe657c10a2656a64df62009007725c6
Antonio Gennaro <antgennaro@hotmail.com>
2023-08-18 15:51:57 -0300

    Maq. TragaMonedas II rel2 -Mejorada 3x24

--- manejo.js
+++ manejo.js
@@ -1,16 +1,29 @@
+// MAQUINA TRAGAMONEDAS II - Release 2 (3 x 24 figuras)
+// con 16 figuras distintas tomadas de una librería de 32...
+const apuestaVal  = document.querySelector("#ap-valor");
 const resultado   = document.querySelector("#result");
-const symbols     = ["🍊", "🍒", "🍋", "🍇", "🍉", "🍓", "🍍", "💎"];
+const miPremio    = document.querySelector("#mi-premio");
+const miSaldo     = document.querySelector("#mi-saldo");
+// librería de símbolos -- posición 0 fija...
+const symbLib     = [ "💎", "🍀", "🔔", "🎰", "🌟", "💰", "🕰️", "📀",
+                      "🍊", "🍒", "🍋", "🍇", "🍉", "🍓", "🍎", "🍍",
+                      "🍄", "🎲", "🌈", "🎁",  "🎩","📯", "🎸", "🎹",
+                      "⚽", "🏈", "🏉", "🥎", "🏀", "🏐", "🎾", "🎱" ];
+
 const maquina     = document.querySelector(".slot-machine");
 const linea1      = document.querySelector(".line1");
 const linea2      = document.querySelector(".line2");
 const linea3      = document.querySelector(".line3");
 const spinButton  = document.querySelector("#spin-button");
 
-// DEFINICIÓN DE PREMIOS (la probabilidad al 90%, el 10% queda para la máquina)
-const DIAMANTESX3 = 450; // 1/8 * 1/8 * 1/8 * 0,9 = Multiplica la apuesta  
-const FIGURASX3   = 50;  // 1/8 * 1/8 * 0,9 
-const DIAMANTESX2 = 25;  // 1/8 * (2*1/8) *.9
-const FIGURASX2   = 3;   // 2 * 1/8 * 0,9
+// DEFINICIÓN DE PREMIOS (la probabilidad al 98,5%, el 1,5% queda para la máquina)
+// el premio Multiplica el valor de la apuesta
+const DIAMANTESX3 = 3000; // 1/(24*24*24)   
+const FIGURASX3   = 130;  // (1/(24*24) - (1/(24*24*24)) 
+const DIAMANTESX2 = 40;   // 3*23/(24*24*24)
+const FIGURASX2   = 2;    // 3/24 - 3*23/(24*24*24)
+// Se eligieron 24 figuras para poder pagar un premio mínimo de 2 veces lo apostado cuando 
+// salen dos figuras iguales por lo que el apostador recupera la apuesta y gana otro tanto.
 
 // ARMADO DE MÁQUINA DE 3 RUEDAS QUE GIRAN, CADA UN NÚMERO ALEATORIO  
 // DE VUELTAS Y CON UN NÚMERO VARIABLE DE FIGURAS   
@@ -18,19 +31,38 @@ const FIGURASX2   = 3;   // 2 * 1/8 * 0,9
 let   rueda1      = [];   // cada rueda tiene el nro_figuras que se 
 let   rueda2      = [];   // cargan aleatoriamente al inicio
 let   rueda3      = [];   // cada vez que se da Girar
-let   nro_figuras = 10;   // 7 + random de 8 => de 7 a 14 => promedio 10)
+let   nro_figuras = 32;   // 24 + random de 12+1 => de 24 a 36 => promedio 32)
 
+// define el array de 24 figuras que luego se llena con el "💎" en en posición 1 
+// fijo y en forma aleatoria 23 mas tomados al azar sin repetir....
+const symbols     = ["💎", "🍒", "🍋", "🍇", "🍉", "🍓", "🍍", "🍎",
+                     "🍀", "🔔", "⚽", "🌟",  "💰", "🎱", "🕰️", "📀", 
+                     "🍄", "🎲", "🌈", "🎁", "🎩", "📯", "🎸", "🎹"];
+let index = Math.floor(Math.random()*symbLib.length); // nro de 0 a symlib.lenght
+// carga de la librería a paetir de una posición módulo longitud de la librería salteando la pos "0" 
+for (let i=1;i<symbols.length;i++){
+  if (++index >= symbLib.length) {index = 1;}
+  symbols[i] = symbLib[index];
+}
+console.log(symbols);
 // ARMADO DE LA PANTALLA DE LA MÁQUINA - MUESTRA SOLO 3 LINEAS 
 // SE TOMAN DE LAS 3 RUEDAS EN FROMA ROTATIVA HASTA QUE SE DETENGA LA ÚLTIMA
 // El resultado se evalúa en la 2fa fila (symbols 2)
 let symbols1  = ["🍒", "🍓", "🍋"];
-let symbols2  = ["🍇", "🍍", "🍓"];
+let symbols2  = ["🍇", "🍍", "🍓"]; // <<< Linea que se evalúa
 let symbols3  = ["🍊", "💎", "🍍"];
 
 // VARIABLES GENERALES
 let spinning = false;
 let symbIndx = 0; 
 let contador = 0;
+
+// contadores estadísticos
+let ciclos     = 0;
+let contDiam_3 = 0;
+let contFig_3  = 0;
+let contDiam_2 = 0;
+let contFig_2  = 0;
 
 // VARIABLES DE JUEGO
 let tipoCambio  = 10;
@@ -39,20 +71,22 @@ let saldoPesos  = 0;
 let apuesta     = 1;
 let premio      = 0;
 
-
 // INICIO
-resultado.textContent = "Intenta una vez grátis. Dale hacela Girar";
+resultado.textContent   = "Intenta una vez grátis. Dale hacela Girar";
+apuestaVal.textContent  = "Apuesta: "+apuesta+" devcoin/s";
 
+// FUNCIONES 
 function getRandomSymbol() {
   const randomIndex = Math.floor(Math.random() * symbols.length);
   return symbols[randomIndex];
 }
 
-function initRuedas() { // por cada intento genera un número de entre 7 y 14 figuras por rueda
+// construye 3 ruedas iguales con un número variable de 24 a 36 figuras   
+function initRuedas() { // por cada intento genera un número de entre 24 y 36 figuras por rueda
   rueda1.length = 0;    // primero vacía los 3 arrays.
   rueda2.length = 0;
   rueda3.length = 0;
-  nro_figuras = 7 + (Math.floor(Math.random() * symbols.length)); // genera el nro entre 7 y 14 
+  nro_figuras = symbols.length + (Math.floor(Math.random() * (symbols.length/2)+1)); // genera el nro entre 24 y 32 
   for (let i=0; i < nro_figuras; i++) {
     rueda1.push (getRandomSymbol());  // carga incialmente en forma aleatoria...
     rueda2.push (getRandomSymbol());  // las 3 ruedas con nro_figuras
@@ -67,34 +101,30 @@ function fillSymbols(symArray) {      // llena el array de simbolos que se muest
     symbIndx = (symbIndx+1) %nro_figuras;
   }
 
+function fillLinea(linea, symArray) {
+  linea.innerHTML = `    
+    <div class="reel" id="reel11">${symArray[0]}</div>
+    <div class="reel" id="reel12">${symArray[1]}</div>
+    <div class="reel" id="reel13">${symArray[2]}</div>
+    `;
+}
+
 function muestraRuedas() {
-  fillSymbols(symbols1);  // llena la priemra línea y muestra
-  linea1.innerHTML = `    
-    <div class="reel" id="reel11">${symbols1[0]}</div>
-    <div class="reel" id="reel12">${symbols1[1]}</div>
-    <div class="reel" id="reel13">${symbols1[2]}</div>
-  `;
-  fillSymbols(symbols2);  // llena la segunda línea y muestra
-  linea2.innerHTML = `
-    <div class="reel" id="reel21">${symbols2[0]}</div>
-    <div class="reel" id="reel22">${symbols2[1]}</div>
-    <div class="reel" id="reel23">${symbols2[2]}</div>
-  `;
-  fillSymbols(symbols3);  // llena la tercera línea y muestra
-  linea3.innerHTML = `
-    <div class="reel" id="reel31">${symbols3[0]}</div>
-    <div class="reel" id="reel32">${symbols3[1]}</div>
-    <div class="reel" id="reel33">${symbols3[2]}</div>
-  `;
-  }
+  fillSymbols(symbols1);        // llena la priemra línea del array desde las ruedas
+  fillLinea (linea1, symbols1); // y muestra la 1ra linea
+  fillSymbols(symbols2);        // llena la segunda línea 
+  fillLinea (linea2, symbols2); // y muestra la 2da linea
+  fillSymbols(symbols3);        // llena la tercera línea 
+  fillLinea (linea3, symbols3); // y muestra la 3da linea
+}
 
 function giraRueda(rueda) { // se llama rueda por rueda 
   let spins = 0;            // y genera un número aleatorio
-  let maxSpins = 36 + Math.floor(Math.random() * 15); // de 36 a 50...
+  let maxSpins = 40 + Math.floor(Math.random() * 25); // de 40 a 64 vueltas...
   function doSpin() {       // para luego ir llenado los arrays 
     for(let i=0;i<nro_figuras; i++) {
-    rueda[i] = getRandomSymbol(); // va llenando en forma aleatoria todas las posiciones
-    }                             // de cada rueda de 36 a 50 veces...
+      rueda[i] = getRandomSymbol(); // va llenando en forma aleatoria todas las posiciones de la rueda (de 8 a 16)
+    } // la cantidad de vueltas van de 2,5 a 4 si son de 16 figuras o de 5 a 8 si son de 8 figuras..
     muestraRuedas();
     spins++;
     if (spins < maxSpins) {
@@ -118,31 +148,41 @@ function checkWin() {
     const symbol1 = symbols2[0];
     const symbol2 = symbols2[1];
     const symbol3 = symbols2[2];
-    // console.log("S1: "+symbol1+"S2: "+symbol2+"S3: "+symbol3);
+    saldoDevC -= apuesta;
+    // Primero busca 3 💎
     if (symbol1 === symbol2 && symbol2 === symbol3) {
-      if (symbol1 === "💎") {premio = apuesta * DIAMANTESX3 }
-      else { premio = apuesta * FIGURASX3}
+      if (symbol1 === "💎") 
+        {premio = apuesta * DIAMANTESX3; 
+         contDiam_3++; }
+      else { premio = apuesta * FIGURASX3;
+            contFig_3++;}
       saldoDevC += premio;
-      console.log(premio+" , "+saldoDevC);
       resultado.textContent ="¡FELICIDADES!!! Tres figuras iguales.";
     } else if (symbol1 === symbol2 || symbol2 === symbol3 || symbol1 === symbol3) {
       if ((symbol1 === "💎")&&(symbol2 === "💎") || (symbol2 === "💎")&&(symbol3 === "💎")|| 
-            (symbol1 === "💎")&&(symbol3 === "💎")) {premio = apuesta * DIAMANTESX2 }
-      else { premio = apuesta * FIGURASX2}
+            (symbol1 === "💎")&&(symbol3 === "💎")) {
+                premio = apuesta * DIAMANTESX2;
+                contDiam_2++; }
+      else { premio = apuesta * FIGURASX2;
+              contFig_2++;}
       saldoDevC += premio;
-      console.log(premio+" , "+saldoDevC);
       resultado.textContent = "¡Ganaste! --- Dos figuras iguales.";
     } else {
       premio = 0;
-      saldoDevC -= apuesta;
-      console.log(premio+" , "+saldoDevC);
       resultado.textContent = "Intenta de nuevo, No hubo coincidencias.";
     }
+    console.log("Ciclo: "+ciclos+" Premio: "+premio+" Saldo: "+saldoDevC+" "+symbol1+symbol2+symbol3);
+    if (ciclos %100 == 0) {
+      console.log("Diam3: "+contDiam_3+" Fig3: "+contFig_3+" Diam2: "+contDiam_2+" Fig2: "+contFig_2);
+    }
+    miPremio.textContent  = "Ganaste: "+premio+" devcoins";
+    miSaldo.textContent   = "Saldo: "+saldoDevC+" devcoins";    
   }
 }
 
 function girarRuedas() {
   if (!spinning) {
+    ciclos++;
     spinButton.style.color = "yellow";
     resultado.textContent = "Ahí vamos... Suerte!!!";
     spinning = true;
@@ -152,19 +192,29 @@ function girarRuedas() {
     giraRueda(rueda3);
     setTimeout(() => {
       spinning = false;
-    }, 2000);
+    }, 1500);
   }
 }
 
+let giraInterval = 0;
+
 function apuesta1() {
   apuesta = 1;
+  apuestaVal.textContent  = "Apuesta: "+apuesta+" devcoin/s";
 }
 function apuesta2() {
-  apuesta = 2;
+  // GENERA CONTINUO....
+  giraInterval = setInterval(girarRuedas, 2000); // 
+  // apuesta = 2;
 }
 function apuesta5() {
-  apuesta = 5;
+  //apuesta = 5;
+  clearInterval(giraInterval);
 }
 function apuesta10() {
   apuesta = 10;
+  apuestaVal.textContent  = "Apuesta: "+apuesta+" devcoin/s";
 }
+
+
+
